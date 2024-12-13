60f176aa1ea48fe49ea216ef5e7d6350782a7fc3
Daniel Rodrigues <capau.2020120iipi0064@aluno.ifpi.edu.br>
2024-12-13 10:00:33 -0300

    Biblioteca criada, pronto para ser desenvolvida

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # POO-Library
+
+pip install dist/transfer_info-0.1.0-py3-none-any.whl
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="transfer_info",
+    version="0.1.0",
+    description="Biblioteca para transferir dados do Redis ao PostGreSQL",
+    author="Daniel, Francinaldo, Luis, Rita, Iago, Cristina",
+    packages=find_packages(),
+    install_requires=[
+        "redis",
+    ],
+    python_requires=">=3.9.0",
+)
